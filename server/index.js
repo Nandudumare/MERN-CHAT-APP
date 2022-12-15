0b01e803cad54fa772fc78b00cca455803487f7b
Nandu Dumare <dumarenandu70@gmail.com>
2022-12-15 21:53:19 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-chat-app-oj4j.vercel.app",
   },
 });
 
